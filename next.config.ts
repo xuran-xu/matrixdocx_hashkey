f62ffed5f92aae18fc914b39a9d4303d2c0b6467
SpectreMercury <francislee920217@gmail.com>
2025-02-26 04:33:56 -0700

    fuck: add rules in next remove eslint fucking settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreDuringBuilds: true,
+  eslint: {
+    // ESLint 检查不会导致构建失败
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
