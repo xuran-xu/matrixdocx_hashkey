8cf9dae76a6bd012a656f5dfdd4f24dda3798461
leo.shi <554601845@qq.com>
2025-03-19 18:58:20 +0800

    更换 unstakeLocked 修改之后的合约地址

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,6 +1,6 @@
 export const contractAddresses = {
     133: {
-      stakingContract: '0x87211876e821A3e5B6372D9c8d9a172024129288' as `0x${string}`,
+      stakingContract: '0x7E850cB78a364285e1dE52682997399E7405baDD' as `0x${string}`,
       stHSKToken: '0x' as `0x${string}`,
     },
     177: {
