9a2e3a9b29d5a0c881ab4fe0d380989a8c0cd98f
leo.shi <554601845@qq.com>
2025-03-19 18:10:55 +0800

    新合约地址

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,6 +1,6 @@
 export const contractAddresses = {
     133: {
-      stakingContract: '0x5dD169019918c34243446D58c837223213331504' as `0x${string}`,
+      stakingContract: '0x87211876e821A3e5B6372D9c8d9a172024129288' as `0x${string}`,
       stHSKToken: '0x' as `0x${string}`,
     },
     177: {
